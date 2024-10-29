043288f9b70efeec0c80b0365615c097f45bd130
Jochen Ullrich <kontakt@ju-hh.de>
2024-10-29 20:06:26 +0100

    feat: Shrink PCB by 16mm

--- scripts/make_pcb_preview.sh
+++ scripts/make_pcb_preview.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd pcb
-pcbdraw plot --side front -l custom --components -m remap.json -s set-black-hasl Gaggimate.kicad_pcb ../docs/assets/pcb_top.png
-pcbdraw plot --side back -l custom --components -m remap.json -s set-black-hasl Gaggimate.kicad_pcb ../docs/assets/pcb_bottom.png
+pcbdraw plot --side front --components -m remap.json -s set-black-hasl Gaggimate.kicad_pcb ../docs/assets/pcb_top.png
+pcbdraw plot --side back --components -m remap.json -s set-black-hasl Gaggimate.kicad_pcb ../docs/assets/pcb_bottom.png
 sips -r 180 ../docs/assets/pcb_bottom.png
