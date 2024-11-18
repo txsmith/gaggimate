bf4bf51984204c87b31da4af93481abd72b414ab
Jochen Ullrich <kontakt@ju-hh.de>
2024-11-18 22:51:30 +0100

    feat: Add squareline project

---
+++ ui/GaggiMate_events.py
@@ -0,0 +1,72 @@
+
+def onLoadStarted(event_struct):
+    return
+
+
+def onBrewScreen(event_struct):
+    return
+
+
+def onWaterScreen(event_struct):
+    return
+
+
+def onSteamScreen(event_struct):
+    return
+
+
+def onStandby(event_struct):
+    return
+
+
+def onBrewCancel(event_struct):
+    return
+
+
+def onBrewStart(event_struct):
+    return
+
+
+def onBrewTempLower(event_struct):
+    return
+
+
+def onBrewTempRaise(event_struct):
+    return
+
+
+def onBrewTimeLower(event_struct):
+    return
+
+
+def onBrewTimeRaise(event_struct):
+    return
+
+
+def onSteamToggle(event_struct):
+    return
+
+
+def onSteamTempLower(event_struct):
+    return
+
+
+def onSteamTempRaise(event_struct):
+    return
+
+
+def onWaterToggle(event_struct):
+    return
+
+
+def onWaterTempLower(event_struct):
+    return
+
+
+def onWaterTempRaise(event_struct):
+    return
+
+
+def onWakeup(event_struct):
+    return
+
