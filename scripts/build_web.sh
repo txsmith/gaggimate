1cf52344a913aa202bd634e3b001a90bf8162551
Jochen Ullrich <kontakt@ju-hh.de>
2024-11-28 00:57:11 +0100

    fix: Fix style css

--- scripts/build_web.sh
+++ scripts/build_web.sh
@@ -7,5 +7,4 @@ cat ./web/index.html > data/index.html
 cat ./web/settings.html > data/settings.html
 cat ./web/ota.html > data/ota.html
 cp web/assets/gm.svg data/assets/gm.svg
-npx minify ./web/assets/style.min.css > data/assets/style.min.css
-sed -i '' 's/%/%%/g' data/assets/style.min.css
+cp ./web/assets/style.min.css data/assets/style.min.css
