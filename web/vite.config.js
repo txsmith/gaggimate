00a2ecc13e80c4f0a9f78ff2fdad904dd27892dd
Jochen Ullrich <kontakt@ju-hh.de>
2025-01-11 10:34:06 +0100

    fix: Revert proxy hostname

--- web/vite.config.js
+++ web/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://silvia.local/',
+        target: 'http://gaggimate.local/',
         changeOrigin: true,
       },
     },
