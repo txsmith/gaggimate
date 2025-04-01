fd4c9edf0ea9234e8d024a5acfb707167f082e18
Jochen Niebuhr <kontakt@ju-hh.de>
2025-04-01 21:03:31 +0200

    feat: Automatically run autotune on machine startup (#167)
    * feat: Automatically startup heater in autotune mode
    
    * fix: Fix board value

--- lib/GaggiMateController/src/peripherals/Heater.cpp
+++ lib/GaggiMateController/src/peripherals/Heater.cpp
@@ -1,37 +1,43 @@
 #include "Heater.h"
 #include <Arduino.h>
 
-Heater::Heater(TemperatureSensor *sensor, int heaterPin, const heater_error_callback_t &error_callback)
+Heater::Heater(TemperatureSensor *sensor, uint8_t heaterPin, const heater_error_callback_t &error_callback)
     : sensor(sensor), heaterPin(heaterPin), taskHandle(nullptr), error_callback(error_callback) {
     pid = new QuickPID(&temperature, &output, &setpoint);
+    tune = new sTune(&temperature, &output, sTune::ZN_PID, sTune::directIP, sTune::printSUMMARY);
 
     output = 0.0f;
-    pid->SetOutputLimits(0, TUNER_OUTPUT_SPAN);
-    pid->SetSampleTimeUs(250 * 1000);
-    pid->SetMode(QuickPID::Control::automatic);
-    pid->SetProportionalMode(QuickPID::pMode::pOnError);
-    pid->SetDerivativeMode(QuickPID::dMode::dOnMeas);
-    pid->SetAntiWindupMode(QuickPID::iAwMode::iAwClamp);
-    pid->SetTunings(Kp, Ki, Kd);
 }
 
 void Heater::setup() {
     pinMode(heaterPin, OUTPUT);
+    setupAutotune();
     xTaskCreate(loopTask, "Heater::loop", configMINIMAL_STACK_SIZE * 4, this, 1, &taskHandle);
+}
+
+void Heater::setupPid() {
+    tune->Configure(0, 0, 0, 0, TEST_TIME_SEC, 0, TUNER_SAMPLES);
+    pid->SetOutputLimits(0, TUNER_OUTPUT_SPAN);
+    pid->SetSampleTimeUs((TUNER_OUTPUT_SPAN - 1) * 1000);
+    pid->SetMode(QuickPID::Control::automatic);
+    pid->SetProportionalMode(QuickPID::pMode::pOnMeas);
+    pid->SetAntiWindupMode(QuickPID::iAwMode::iAwClamp);
+    pid->SetTunings(Kp, Ki, Kd);
+}
+
+void Heater::setupAutotune() {
+    tune->Configure(TUNER_INPUT_SPAN, TUNER_OUTPUT_SPAN, outputStart, outputStep, TEST_TIME_SEC_AUTOTUNE, SETTLE_TIME_SEC_AUTOTUNE, TUNER_SAMPLES_AUTOTUNE);
+    tune->SetEmergencyStop(120);
 }
 
 void Heater::loop() {
     if (temperature <= 0 || setpoint <= 0) {
-        analogWrite(heaterPin, 0);
+        digitalWrite(heaterPin, LOW);
         temperature = sensor->read();
         return;
     }
 
-    if (pid->Compute()) {
-        float rawTemp = sensor->read();
-        temperature = 0.2 * rawTemp + 0.8 * temperature;
-        analogWrite(heaterPin, output);
-    }
+    loopAutotune();
 }
 
 void Heater::setSetpoint(float setpoint) {
@@ -40,9 +46,56 @@ void Heater::setSetpoint(float setpoint) {
 }
 
 void Heater::setTunings(float Kp, float Ki, float Kd) {
+    /*
     if (pid->GetKp() != Kp || pid->GetKi() != Ki || pid->GetKd() != Kd) {
         pid->SetTunings(Kp, Ki, Kd);
         ESP_LOGV(LOG_TAG, "Set tunings to Kp: %f, Ki: %f, Kd: %f", Kp, Ki, Kd);
+    }
+    */
+}
+
+void Heater::loopPid() {
+    float optimumOutput = tune->softPwm(heaterPin, temperature, output, setpoint, TUNER_OUTPUT_SPAN, 0);
+    if (pid->Compute()) {
+        float rawTemp = sensor->read();
+        temperature = 0.2 * rawTemp + 0.8 * temperature;
+        tune->plotter(temperature, optimumOutput, setpoint, 0.1f, 3);
+    }
+}
+
+void Heater::loopAutotune() {
+    float rawTemp = 0;
+    float optimumOutput = tune->softPwm(heaterPin, temperature, output, setpoint, TUNER_OUTPUT_SPAN, debounce);
+    switch (tune->Run()) {
+        case tune->sample: // active once per sample during test
+            rawTemp = sensor->read();
+            temperature = 0.2 * rawTemp + 0.8 * temperature;
+            tune->plotter(temperature, output, setpoint, 0.1f, 3); // output scale 0.5, plot every 3rd sample
+            break;
+        case tune->tunings: // active just once when sTune is done
+            tune->GetAutoTunings(&Kp, &Ki, &Kd); // sketch variables updated by sTune
+            pid->SetOutputLimits(0, TUNER_OUTPUT_SPAN);
+            pid->SetSampleTimeUs((TUNER_OUTPUT_SPAN - 1) * 1000);
+            debounce = 0; // ssr mode
+            output = outputStep;
+            pid->SetMode(QuickPID::Control::automatic);
+            pid->SetProportionalMode(QuickPID::pMode::pOnMeas);
+            pid->SetAntiWindupMode(QuickPID::iAwMode::iAwClamp);
+            pid->SetTunings(Kp, Ki, Kd);
+            break;
+
+        case tune->runPid: // active once per sample after tunings
+            if (startup && temperature > setpoint - 5) { // reduce overshoot
+                startup = false;
+                output -= 9;
+                pid->SetMode(QuickPID::Control::manual);
+                pid->SetMode(QuickPID::Control::automatic);
+            }
+            rawTemp = sensor->read();
+            temperature = 0.2 * rawTemp + 0.8 * temperature;
+            pid->Compute();
+            tune->plotter(temperature, optimumOutput, setpoint, 0.1f, 3);
+            break;
     }
 }
 
--- lib/GaggiMateController/src/peripherals/Heater.h
+++ lib/GaggiMateController/src/peripherals/Heater.h
@@ -1,33 +1,42 @@
 #ifndef HEATER_H
 #define HEATER_H
 #include <QuickPID.h>
+#include <sTune.h>
 
 #include "FreeRTOS.h"
 #include "Max31855Thermocouple.h"
 #include "TemperatureSensor.h"
 #include <task.h>
 
-constexpr int HEATER_UPDATE_INTERVAL = 250;
-constexpr float TUNER_INPUT_SPAN = 0.0f;
-constexpr float TUNER_OUTPUT_SPAN = 255.0f;
+constexpr float TUNER_INPUT_SPAN = 150.0f;
+constexpr float TUNER_OUTPUT_SPAN = 1000.0f;
 constexpr uint32_t TEST_TIME_SEC = 1;
 constexpr uint16_t TUNER_SAMPLES = 1;
+constexpr uint32_t TEST_TIME_SEC_AUTOTUNE = 300;
+constexpr uint16_t TUNER_SAMPLES_AUTOTUNE = 1200;
+constexpr uint16_t SETTLE_TIME_SEC_AUTOTUNE = 10;
 
 using heater_error_callback_t = std::function<void()>;
 
 class Heater {
   public:
-    Heater(TemperatureSensor *sensor, int heaterPin, const heater_error_callback_t &error_callback);
+    Heater(TemperatureSensor *sensor, uint8_t heaterPin, const heater_error_callback_t &error_callback);
     void setup();
+    void setupPid();
+    void setupAutotune();
     void loop();
     void setSetpoint(float setpoint);
     void setTunings(float Kp, float Ki, float Kd);
 
   private:
+    void loopPid();
+    void loopAutotune();
+
     TemperatureSensor *sensor;
-    int heaterPin;
+    uint8_t heaterPin;
     xTaskHandle taskHandle;
     QuickPID *pid;
+    sTune *tune;
 
     heater_error_callback_t error_callback;
 
@@ -37,6 +46,13 @@ class Heater {
     float Kp = 2.4;
     float Ki = 40;
     float Kd = 10;
+
+
+    // Autotune variables
+    float outputStart = 0;
+    float outputStep = 500;
+    uint8_t debounce = 1;
+    bool startup = true;
 
     const char *LOG_TAG = "Heater";
     static void loopTask(void *arg);
