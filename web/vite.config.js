659d90f701e2f314caaaaf5c98bcedc0cc13f705
Jochen Ullrich <kontakt@ju-hh.de>
2025-06-13 10:42:19 +0200

    Revert "fix: Shrink bundle size by 6x"
    This reverts commit 8bce6a6545d4846e634678c21b02e69316e532b2.

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
-import analyzer, { adapter } from 'vite-bundle-analyzer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  build: {
-    rollupOptions: {
-      treeshake: "smallest"
-    }
-  },
 
   server: {
     proxy: {
